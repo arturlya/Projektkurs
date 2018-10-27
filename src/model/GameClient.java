4a4349662908c064266339c3d7f42f85e07baa90
Marcel <mbraun0301@gmail.com>
2018-10-27 21:04:28 +0200

    JavaDoc

--- src/model/GameClient.java
+++ src/model/GameClient.java
@@ -3,6 +3,7 @@ package model;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.IUpdateable;
 import de.gurkenlabs.litiengine.entities.IEntity;
+import de.gurkenlabs.litiengine.graphics.RenderType;
 import de.gurkenlabs.litiengine.input.Input;
 import model.Screens.IngameScreen;
 import model.abitur.datenstrukturen.List;
@@ -20,6 +21,13 @@ public class GameClient extends Client implements IUpdateable {
 
     private List<Player> others;
 
+    /**
+     * Konstruktor der Klasse GameClient.
+     * Besitzt einen KeyListener zum voten.
+     *
+     * @param ip IP des Servers
+     * @param port Port des Servers
+     */
     public GameClient(String ip, int port){
         super(ip,port);
         ready = false;
@@ -54,6 +62,9 @@ public class GameClient extends Client implements IUpdateable {
 
     }
 
+    /**
+     * Update des Interfaces IUpdateable
+     */
     @Override
     public void update(){
         if(player != null)
@@ -68,92 +79,103 @@ public class GameClient extends Client implements IUpdateable {
     }
 
 
-
-
-
+    /**
+     * Verarbeitet die empfangene Nachricht vom Server.
+     *
+     * @param pMessage Die empfangene Nachricht, wenn
+     *                 -pMessage mit "START" dann beginnt das Spiel
+     *                 -pMessage mit "PLAYER" dann wird eine Spielernummer vom Server zugewiesen
+     *                 //-pMessage mit "CHOOSE"
+     *                 -pMessage mit "ALL" bekommt der Client alle Spieler vom Server
+     *                 -pMessage mit "POSITION" bekommt der Client die Position eines bestimmten Clients und aktualisiert sie bei sich
+     */
     @Override
-    public void processMessage(Object pMessage) {
+    public void processMessage(String pMessage) {
         int max = 0;
         if(max == 0) {
-            if(pMessage.getClass().getName().equals("java.lang.String")) {
-                String message = pMessage.toString();
-                if (message.contains("START")) {
-                    String[] temp = message.split("START", 2);
-                    if (temp[1].contains("true")) {
-                        gameStarted = true;
-                    } else {
-                        gameStarted = false;
-                    }
-                } else if (message.contains("PLAYER")) {
-                    String[] temp = message.split("PLAYER");
-                    playerNumber = Integer.parseInt(temp[1]);
-
+            if (pMessage.contains("START")) {
+                String[] temp = pMessage.split("START", 2);
+                if (temp[1].contains("true")) {
+                    gameStarted = true;
+                } else {
+                    gameStarted = false;
+                }
+            } else if (pMessage.contains("PLAYER")) {
+                String[] temp = pMessage.split("PLAYER");
+                playerNumber = Integer.parseInt(temp[1]);
                     //choosePlayer(1);
-                } else if(message.contains("CHOOSE")){
-                    //choosePlayer(1);
-                } else if (message.contains("ALL")) {
-                    String[] temp = message.split("ALL");
-                    temp = temp[1].split("NEXT");
-                    if(getNumberOfOtherPlayers() < Integer.parseInt(temp[0])) {
-                        for (int i = 1; i < temp.length; i++) {
-                            //Aufpassen temp0
-                            String[] charInfo = temp[i].split("#", 2);
-                            if (Integer.parseInt(charInfo[0]) != player.getPlayerNumber()) {
-                                if (charInfo[1].equals("Warrior")) {
-                                    Player otherPlayer = new Warrior(false);
-                                    others.append(otherPlayer);
-                                    Game.getEnvironment().add(otherPlayer);
-                                    System.out.println("Added player");
-                                } else if (charInfo[1].equals("Mage")) {
-                                    Player otherPlayer = new Mage(false);
-                                    others.append(otherPlayer);
-                                    Game.getEnvironment().add(otherPlayer);
-                                    System.out.println("Added player");
-                                }
+            } else if(pMessage.contains("CHOOSE")){
+                //choosePlayer(1);
+            } else if (pMessage.contains("ALL")) {
+                String[] temp = pMessage.split("ALL");
+                temp = temp[1].split("NEXT");
+                if(getNumberOfOtherPlayers() < Integer.parseInt(temp[0])) {
+                    for (int i = 1; i < temp.length; i++) {
+                        //Aufpassen temp0
+                        String[] charInfo = temp[i].split("#", 2);
+                        if (Integer.parseInt(charInfo[0]) != player.getPlayerNumber()) {
+                            if (charInfo[1].equals("Warrior")) {
+                                Player otherPlayer = new Warrior(false);
+                                others.append(otherPlayer);
+                                Game.getEnvironment().add(otherPlayer);
+                                Game.getEnvironment().add(otherPlayer, RenderType.NORMAL);
+                                //ingameScreen.addGravObject(otherPlayer);
+                                System.out.println("Added player");
+                            } else if (charInfo[1].equals("Mage")) {
+                                Player otherPlayer = new Mage(false);
+                                others.append(otherPlayer);
+                                Game.getEnvironment().add(otherPlayer);
+                                Game.getEnvironment().add(otherPlayer,RenderType.NORMAL);
+                                //ingameScreen.addGravObject(otherPlayer);
+                                System.out.println("Added player");
                             }
                         }
                     }
-
-                } else if (message.contains("POSITION")) {
-                    String[] temp = message.split("POSITION");
-                    temp = temp[1].split("#");
-                    if (Integer.parseInt(temp[0]) != player.getPlayerNumber()) {
-
-                        int posInList = Integer.parseInt(temp[0]) - 1;
-                        others.toFirst();
-                        while (posInList > 0) {
-                            others.next();
-                            posInList--;
-                        }
-
-                        if (others.hasAccess()) {
-                            others.getContent().setX(Double.parseDouble(temp[1]));
-                            others.getContent().setY(Double.parseDouble(temp[2]));
-                           // System.out.println("Updating");
-                        }
+                }
+            } else if (pMessage.contains("POSITION")) {
+                String[] temp = pMessage.split("POSITION");
+                temp = temp[1].split("#");
+                if (Integer.parseInt(temp[0]) != player.getPlayerNumber()) {
+                    int posInList = Integer.parseInt(temp[0]) - 1;
+                    others.toFirst();
+                    while (posInList > 0) {
+                        others.next();
+                        posInList--;
                     }
-
+                    if (others.hasAccess()) {
+                        others.getContent().setX(Double.parseDouble(temp[1]));
+                        others.getContent().setY(Double.parseDouble(temp[2]));
+                        // System.out.println("Updating");
+                    }
                 }
             }
-
             max++;
         }
     }
 
 
+    /**
+     * Methode zum Initialisieren.
+     */
     protected void init(){
         List<IEntity> entityList = new List<>();
         entityList.append(player);
         entityList.toFirst();
         while(entityList.hasAccess()){
             Game.getEnvironment().add(entityList.getContent());
-            /*if(entityList.getContent() instanceof GravitationalObject){
+            //Game.getEnvironment().add(e);
 
-            }*/
+            if(entityList.getContent() instanceof GravitationalObject){
+                Game.getEnvironment().add((GravitationalObject)entityList.getContent(),RenderType.NORMAL);
+                //ingameScreen.addGravObject((GravitationalObject)entityList.getContent());
+            }
             entityList.next();
         }
     }
 
+    /**
+     * @return Gibt die Anzahl an gegnerischen Spielern, die der Client kennt.
+     */
     private int getNumberOfOtherPlayers(){
         int value = 0;
         others.toFirst();
@@ -165,6 +187,12 @@ public class GameClient extends Client implements IUpdateable {
         return value;
     }
 
+    /**
+     * Sucht einen Spieler aus.
+     * Sollte ein Spieler ausgesucht sein, sendet er bei erneutem Aufruf dem Server die Zahl des Spielers
+     *
+     * @param number Index des gewünschten Spielers
+     */
     private void choosePlayer(int number){
         if(player == null) {
             switch (number) {
@@ -173,6 +201,8 @@ public class GameClient extends Client implements IUpdateable {
                     break;
                 case 2:
                     player = new Mage(true);
+
+
                     break;
             }
         }else{
