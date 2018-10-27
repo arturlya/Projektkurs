a3d1a53109ddf74016543079aeda3a1f7a4f1181
Artur Lyadskyy <lyadskyyartur@gmail.com>
2018-10-27 16:33:50 +0200

    Map laden

---
+++ src/model/Screens/MenuScreen.java
@@ -0,0 +1,34 @@
+package model.Screens;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.environment.Environment;
+import de.gurkenlabs.litiengine.gui.screens.Screen;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class MenuScreen extends Screen {
+
+    private int menuNumber;
+    private Image[] menuImages;
+
+    public MenuScreen(){
+        super("MENU");
+        menuNumber = 0;
+        menuImages = new Image[4];
+        try {
+            menuImages[0] = ImageIO.read(new File("assets/img/menu.png"));
+        } catch (IOException ex) {
+            System.out.println("Bild konnrte nicht geladen werden!");
+        }
+    }
+
+    public void render(Graphics2D g) {
+        if (menuNumber == 0){
+            //g.drawImage(menuImages[menuNumber],0,0, Game.getConfiguration().graphics().getResolutionWidth(),Game.getConfiguration().graphics().getResolutionHeight(),null);
+        }
+        Game.getEnvironment().render(g);
+    }
+}
