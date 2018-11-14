ff3a5f7875e6d5fd8d7c633b60912e5e2d19cb22
Alex <heimann.alexander@gmx.net>
2018-11-14 13:39:30 +0100

    Hook und Projectile modifiziert, sodass sie von der PhysicsEngine beeinflusst werden

--- src/model/Hurtbox.java
+++ src/model/Hurtbox.java
@@ -5,6 +5,7 @@ import java.awt.geom.Rectangle2D;
 public class Hurtbox extends Rectangle2D.Double {
     private boolean hurting;
     private int damage, knockback;
+    private double relativeX, relativeY;
 
     public Hurtbox(double x, double y, int width, int height){
         super(x,y,width,height);
@@ -32,5 +33,28 @@ public class Hurtbox extends Rectangle2D.Double {
 
     public void setKnockback(int knockback) {
         this.knockback = knockback;
+    }
+
+    public double getRelativeX() {
+        return relativeX;
+    }
+
+    public void setRelativeX(double relativeX) {
+        this.relativeX = relativeX;
+    }
+
+    public double getRelativeY() {
+        return relativeY;
+    }
+
+    public void setRelativeY(double relativeY) {
+        this.relativeY = relativeY;
+    }
+
+    public void setRelativeRect(double relativeX, double relativeY, double width, double height){
+        this.relativeX = relativeX;
+        this.relativeY = relativeY;
+        this.width = width;
+        this.height = height;
     }
 }
