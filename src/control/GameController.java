374e29b22fb176d445b6101aaef86086c6d3578b
Alex <heimann.alexander@gmx.net>
2018-11-18 17:29:45 +0100

    erstellen von physics- und collisioncontroller im gamecontroller

--- src/control/GameController.java
+++ src/control/GameController.java
@@ -3,27 +3,42 @@ package control;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.graphics.RenderType;
 import model.GravitationalObject;
+import model.Mage;
+import model.Maps.Map1;
 import model.Screens.IngameScreen;
 import model.User;
 import model.Warrior;
 
+import java.util.ArrayList;
+
 public class GameController {
+
+    private PhysicsController physicsController;
+
     public GameController(IngameScreen ingameScreen) {
 
+        physicsController = new PhysicsController();
+
         Game.getEnvironment().add(new Timer());
-        Game.getEnvironment().add(new PhysicsController());
+        CollisionController collisionController = new CollisionController(this);
+        Game.getRenderEngine().setBaseRenderScale(1);
 
         //IMap map = Game.getEnvironment().getMap();
-/*
-        Warrior m = new Warrior(true);
-        m.setX(0);
+
+        Mage m = new Mage(true);
+        m.setX(300);
         m.setY(100);
         Game.getEnvironment().add(m);
         Game.getEnvironment().add(m,RenderType.NORMAL);
-*/
-        //ingameScreen.addGravObject(m);
+
+        Warrior w = new Warrior(false);
+        w.setX(400);
+        w.setY(100);
+        Game.getEnvironment().add(w);
+        Game.getEnvironment().add(w,RenderType.NORMAL);
 
 
+/*
         int switchFunction = 2;
 
         if(switchFunction == 1) {
@@ -37,6 +52,10 @@ public class GameController {
             user2.joinGame("localhost", 2796);
             user2.init();
 
-        }
+        }*/
+    }
+
+    public ArrayList<GravitationalObject> getGravObjects(){
+        return physicsController.getGravObjects();
     }
 }
