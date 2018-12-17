3d82dd2edc7a5c484bf3af39086b3431f6ff4548
Marcel <mbraun0301@gmail.com>
2018-12-17 13:12:37 +0100

    Man kann die External IP bekommen

--- src/model/ConnectionClient.java
+++ src/model/ConnectionClient.java
@@ -5,6 +5,12 @@ import de.gurkenlabs.litiengine.IUpdateable;
 import model.Screens.MenuScreen;
 import model.abitur.netz.Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ConnectionClient extends Client implements IUpdateable {
 
     private String serverIP;
@@ -14,7 +20,7 @@ public class ConnectionClient extends Client implements IUpdateable {
     private boolean connected;
 
     public ConnectionClient(User user,int port){
-        super("192.168.178.50",port);
+        super("178.201.129.203",port);
         Game.getLoop().attach(this);
         foundServer = false;
         this.user = user;
@@ -30,7 +36,7 @@ public class ConnectionClient extends Client implements IUpdateable {
     @Override
     public void update() {
         if(MenuScreen.hostPort.length() == 4 && !connected){
-            send("SERVER"+StaticData.ip+"#"+MenuScreen.hostPort);
+            send("SERVER"+getExternalIP()+"#"+MenuScreen.hostPort);
             user.hostGame(Integer.parseInt(MenuScreen.hostPort));
             connected = true;
         }
@@ -46,12 +52,32 @@ public class ConnectionClient extends Client implements IUpdateable {
             String[] temp = pMessage.split("SERVER");
             temp = temp[1].split("#");
             serverIP = temp[0];
+            //serverIP = "178.201.129.203";
             serverPort = Integer.parseInt(temp[1]);
+            System.out.println("Joining "+serverIP+" at "+serverPort);
             user.joinGame(serverIP,serverPort);
             connected = true;
             //foundServer = true;
         }
     }
 
+
+    public String getExternalIP(){
+        String ip = "";
+        URL whatismyip = null;
+        try {
+            whatismyip = new URL("http://checkip.amazonaws.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    whatismyip.openStream()));
+
+            ip = in.readLine(); //you get the IP as a String
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ip;
+    }
 
 }
