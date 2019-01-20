644c3af37a6cba041d11d43227ffaa5792244e7d
Artur Lyadskyy <lyadskyyartur@gmail.com>
2019-01-20 19:09:46 +0100

    Map + bg

--- src/model/Maps/Map.java
+++ src/model/Maps/Map.java
@@ -10,16 +10,13 @@ import java.util.ArrayList;
 
 public class Map extends Entity implements IRenderable {
 
-    Image map;
+    Image map,bg;
     protected int width,height;
     protected ArrayList<Point> spawnpoints;
     ArrayList<Line2D.Double> lines;
 
     public Map(){
-        if (Game.getConfiguration().graphics().isFullscreen()){
-            width = (int)Game.getConfiguration().graphics().getResolution().getWidth();
-            height = (int)Game.getConfiguration().graphics().getResolution().getHeight();
-        }else width = Game.getConfiguration().graphics().getResolutionWidth(); height = Game.getConfiguration().graphics().getResolutionHeight();
+
     }
 
     @Override
