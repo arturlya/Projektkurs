c85f18883640e35796bb1669662f8cfb666362bd
Artur Lyadskyy <lyadskyyartur@gmail.com>
2019-02-04 13:26:48 +0100

    Config

--- src/control/Config.java
+++ src/control/Config.java
@@ -11,8 +11,8 @@ public class Config {
 
     public Config() {
         pref = Preferences.userNodeForPackage(Config.class);
-        StaticData.moveUp = pref.getInt("moveUp",KeyEvent.VK_W);
-        StaticData.moveDown = pref.getInt("moveDown",KeyEvent.VK_S);
+        StaticData.lookUp = pref.getInt("lookUp",KeyEvent.VK_W);
+        StaticData.lookDown = pref.getInt("lookDown",KeyEvent.VK_S);
         StaticData.moveLeft = pref.getInt("moveLeft",KeyEvent.VK_A);
         StaticData.moveRight = pref.getInt("moveRight",KeyEvent.VK_D);
         StaticData.jump = pref.getInt("jump",KeyEvent.VK_UP);
